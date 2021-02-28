f8fbfd96f5330cbc32e32ff08165f84df1467ebe
abu-a <abu-a@DESKTOP-OE2A871>
2021-02-28 21:40:34 +0200

    Add IAirlineDAO with new method + AirlineDAOPGSQL

---
+++ Flightmanagementsystem/AirlineDAOPGSQL.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flightmanagementsystem
+{
+    class AirlineDAOPGSQL : IAirlineDAO
+
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAirlineByUsername()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAllAirlineByCountry()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Flightmanagementsystem/IAirlineDAO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flightmanagementsystem
+{
+    interface IAirlineDAO : IBasicDb<AirlineCompany>
+    {
+        void GetAirlineByUsername();
+        void GetAllAirlineByCountry();
+
+    }
+}
