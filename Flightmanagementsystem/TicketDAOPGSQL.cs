684c10db3dc312703f978c2c70eef5e98406ddbe
abu-a <abu-a@DESKTOP-OE2A871>
2021-02-28 22:11:22 +0200

    Update TicketDAOPGSQL

--- Flightmanagementsystem/TicketDAOPGSQL.cs
+++ Flightmanagementsystem/TicketDAOPGSQL.cs
@@ -1,34 +1,157 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace Flightmanagementsystem
 {
     class TicketDAOPGSQL : ITkketDAO
     {
+        string conn_string = "";
         public void Add()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "AddTicket";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
 
         public void Get()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "GetTicke";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
 
         public void GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "GetTicke";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
 
         public void Remove()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "RemoveTicket";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
 
         public void Update()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "UpdateTicket";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
     }
 }
