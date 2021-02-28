4e2fae0f8a62bef0e8d5b912b1700032467e6ef4
abu-a <abu-a@DESKTOP-OE2A871>
2021-02-28 21:46:26 +0200

    Add ICustomerDAO with new method + CustomerDAOPGSQL

---
+++ Flightmanagementsystem/CustomerDAOPGSQL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flightmanagementsystem
+{
+    class CustomerDAOPGSQL : ICustomerDAO
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetCustomerByUsername()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
