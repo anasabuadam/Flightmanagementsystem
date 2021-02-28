44a224feb48ff977f18974f271c4d56e05ab0fef
abu-a <abu-a@DESKTOP-OE2A871>
2021-02-28 23:40:39 +0200

    Update CustomerDAOPGSQL + SP

--- Flightmanagementsystem/CustomerDAOPGSQL.cs
+++ Flightmanagementsystem/CustomerDAOPGSQL.cs
@@ -1,39 +1,186 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace Flightmanagementsystem
 {
     class CustomerDAOPGSQL : ICustomerDAO
     {
+        string conn_string = "";
         public void Add()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "AddCoustomer";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
 
         public void Get()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "GetCustomer";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
 
         public void GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "GetCustomer";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
 
         public void GetCustomerByUsername()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "GetCustomerByUsername";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
 
         public void Remove()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "RemoveCustomer";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
 
         public void Update()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "UpdateCustomer";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
     }
 }
