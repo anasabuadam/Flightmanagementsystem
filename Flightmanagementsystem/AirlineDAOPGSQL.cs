8851a5117f2cc23e86696f46a3ed54b184f3878d
abu-a <abu-a@DESKTOP-OE2A871>
2021-02-28 23:02:29 +0200

    Update AirlineDAOPGSQL + SP

--- Flightmanagementsystem/AirlineDAOPGSQL.cs
+++ Flightmanagementsystem/AirlineDAOPGSQL.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Text;
 
 namespace Flightmanagementsystem
@@ -7,39 +9,208 @@ namespace Flightmanagementsystem
     class AirlineDAOPGSQL : IAirlineDAO
 
     {
+        string conn_string = "";
         public void Add()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "AddAirline";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
 
         public void Get()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "GetAirline";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
 
         public void GetAirlineByUsername()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "GetAirlineByUsername";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
 
         public void GetAll()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "GetAirline";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
 
         public void GetAllAirlineByCountry()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "GetAirlineByCountry";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
 
         public void Remove()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "RemoveAirline";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
 
         public void Update()
         {
-            throw new NotImplementedException();
+            try
+            {
+
+
+                using (SqlConnection sqlConnection1 = new SqlConnection(conn_string))
+                {
+                    using (SqlCommand cmd = new SqlCommand())
+                    {
+
+                        cmd.CommandText = "UpdateAirline";
+                        cmd.CommandType = CommandType.StoredProcedure;
+                        cmd.Connection = sqlConnection1;
+
+                        sqlConnection1.Open();
+
+                        cmd.ExecuteNonQuery();
+                        SqlDataReader reader = cmd.ExecuteReader(CommandBehavior.Default);
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Failed to run sp from db {ex}");
+            }
         }
     }
 }
