382b4c6812a7e784bfd981a593de9f7919ecf3ec
abu-a <abu-a@DESKTOP-OE2A871>
2021-02-28 21:54:37 +0200

    Add IFlightDAO with new method + FlightDAOPGSQL

---
+++ Flightmanagementsystem/FlightDAOPGSQL.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flightmanagementsystem
+{
+    class FlightDAOPGSQL : IFlightDAO
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAllFlightsVacancy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetFlightByCustomer()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetFlightByDepartureDate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetFlightByDestinationCountry()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetFlightById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetFlightByLandingDate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetFlightByOriginCountry()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
