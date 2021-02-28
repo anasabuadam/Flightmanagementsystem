49c2f54d8d3895844369dfa9975e8548af44d82d
abu-a <abu-a@DESKTOP-OE2A871>
2021-02-28 21:41:09 +0200

    Add ITkketDAO +  class TicketDAOPGSQL

---
+++ Flightmanagementsystem/TicketDAOPGSQL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flightmanagementsystem
+{
+    class TicketDAOPGSQL : ITkketDAO
+    {
+        public void Add()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Get()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
