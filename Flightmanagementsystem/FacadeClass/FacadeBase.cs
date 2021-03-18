ecd28912e3e88f4c68c2740764d70d4d623ed262
abu-a <abu-a@DESKTOP-OE2A871>
2021-03-19 00:49:40 +0200

    Update Project Add Folder For Facade Class And Update Class FacafeBase

---
+++ Flightmanagementsystem/FacadeClass/FacadeBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flightmanagementsystem
+{
+   abstract class FacadeBase
+    {
+        IAirlineDAO airlineDAO = new AirlineDAOPGSQL();
+
+        ICountryDAO countryDAO = new CountryDAOPGSQL();
+        ICustomerDAO customerDAO = new CustomerDAOPGSQL();
+        IFlightDAO flightDAO = new FlightDAOPGSQL();
+        ITicketDAO Ticket = new TicketDAOPGSQL();
+
+    }
+}
---
+++ Flightmanagementsystem/FacadeClass/IAnonymoussUserFacade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flightmanagementsystem
+{
+    interface IAnonymoussUserFacade
+    {
+        void GetAllAirlineCompanies();
+        void GetAllFlights();
+        void GetAllFlightsVacancy();
+        void GetFlightById();
+        void GetFlightsByDepatrureDate();
+        void GetFlightsByDestinationCountry();
+        void GetFlightsByLandingDate();
+        void GetFlightsByOriginCountry();
+
+
+    }
+}
