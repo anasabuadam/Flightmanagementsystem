9bbad72c1e9b4aa03df0f1b84f4f9ccb31ea49cc
abu-a <abu-a@DESKTOP-OE2A871>
2021-03-19 01:04:29 +0200

    Add Class AnonymousUserFacade

---
+++ Flightmanagementsystem/FacadeClass/AnonymousUserFacade.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flightmanagementsystem.FacadeClass
+{
+    class ClassAnonymousUserFacade : IAnonymoussUserFacade
+    {
+       
+        public void GetAllAirlineCompanies()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAllFlights()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetAllFlightsVacancy()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetFlightById()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetFlightsByDepatrureDate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetFlightsByDestinationCountry()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetFlightsByLandingDate()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void GetFlightsByOriginCountry()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
