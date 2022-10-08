99e6032ba3bd4e7d9ffc4e348a7d2a7a08ad1625
AnasAbuRmoz <abu-adam2@hotmail.com>
2022-10-08 20:39:09 +0300

    add

---
+++ Flightmanagementsystem/myapp/frontend/flyapp/src/App.js
@@ -0,0 +1,20 @@
+const { dboperations } = require('../src/dbconfig');
+
+
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var cors = require('cors');
+var app = express();
+const router = express.Router();
+
+
+function App() {
+  return (
+    <div className="App">
+   
+    </div>
+  );
+}
+
+export default App;
