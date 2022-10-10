f91ce51c85e097823c4f3ac1fb8500dc70420fb0
AnasAbuRmoz <abu-adam2@hotmail.com>
2022-10-10 14:42:08 +0300

    dell

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,6 +1,33 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+namespace WebApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-app.MapGet("/", () => "Hello World!");
+            // Add services to the container.
 
-app.Run();
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseAuthorization();
+
+
+            app.MapControllers();
+
+            app.Run();
+        }
+    }
+}
